953c62e9b5db1515d2e14b3e67cf3f7775a10a01
Imtiyaziy <hitzamiy@gmail.com>
2023-09-07 20:32:28 -0700

    writing first program with put()function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - display "programming is like building a multilingual puzzle"
+ * Return: 0 and exit the program
+ */
+int main(void)
+{
+	put("\" programming is like building a multilingual puzzle");
+	return (0);
+}
