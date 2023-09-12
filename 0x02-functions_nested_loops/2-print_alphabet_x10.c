a36c6651aeaf4b345dd9932dc484fc57c9edacd9
Imtiyaziy <hitzamiy@gmail.com>
2023-09-12 14:44:28 -0700

    nested

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,15 +1,16 @@
 #include "main.h"
+
 /**
- *print_alphabet - entry point
+ *print_alphabet_x10 - entry point
  *
  *Return: 0 for success
  */
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
 	int me;
 	int you;
 
-	for (me = 'a'; me <= 'z'; me++)
+	for (me = '0'; me <= '9'; me++)
 	{
 		for (you = 'a'; you <= 'z'; you++)
 			_putchar(you);
