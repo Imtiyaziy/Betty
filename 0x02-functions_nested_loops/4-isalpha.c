85dc29a11eeacd8c0a939d67ce79de067ef125e6
Imtiyaziy <hitzamiy@gmail.com>
2023-09-12 16:08:40 -0700

    g

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *_isalpha - display point
+ *@c: The character t9 print
+ *
+ *Return: 0 for success
+ */
+int _isalpha(int c)
+{
+	if (c >= '65' && c <= '122')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	return (0);
+}
