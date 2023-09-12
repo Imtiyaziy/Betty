90e1237cc49e9dae02bd53dae66154d83dd2061a
Imtiyaziy <hitzamiy@gmail.com>
2023-09-12 15:46:49 -0700

    us

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *_islower - display point
+ *@c: The character t9 print
+ *
+ *Return: 0 for success
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+
+	return (0);
+}
