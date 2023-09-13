24b92b0fafe971dc66708f98330101ed4715a50b
Imtiyaziy <hitzamiy@gmail.com>
2023-09-13 13:16:36 -0700

    jack bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,33 +9,30 @@
 void jack_bauer(void)
 {
 	int h;
+	int h1;
+	int m;
+	int s;
 
-		for (h = 0; h <= 2; h++)
+	for (h = 0; h <= 2; h++)
+	{
+		for (h1 = 0; h1 < 4; h1++)
 		{
-			int h1;
-
-			for (h1 = 0; h1 < 4; h1++)
+			for (m = 0; m <= 5; m++)
 			{
-				int m;
-
-				for (m = 0; m <= 5; m++)
+				for (s = 0; s <= 9; s++)
 				{
-					int s;
-
-					for (s = 0; s <= 9; s++)
+					if (h == 2 && h1 == 3)
 					{
-						if (h == 2 && h1 == 3)
-						{
-							break;
-						}
-						_putchar(h + '0');
-						_putchar(h1 + '0');
-						_putchar(58);
-						_putchar(m + '0');
-						_putchar(s + '0');
+						break;
 					}
+					_putchar(h + '0');
+					_putchar(h1 + '0');
+					_putchar(58);
+					_putchar(m + '0');
+					_putchar(s + '0');
 				}
 			}
 		}
+	}
 }
 
