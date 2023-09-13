8b6adf4940111630c1a7f94a102395809cbd5f12
Imtiyaziy <hitzamiy@gmail.com>
2023-09-13 12:10:20 -0700

    jack bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,12 +1,39 @@
 #iclude "main.h"
 
 /**
- *void - display point
+ *jack_bauer - display point
  *a : The character to print
  *
  *Return: 0 for sucess
  */
-int main()
+void jack_bauer(void)
 {
-	return (0);
+	int h = 0,
+
+		for (d = 0; d <= 2; d++)
+		{
+			int h1;
+
+			for (h1 = 0; h1 <= 3; h1++)
+			{
+				_putchar(h1);
+			}
+			_pucchar(h);
+			_putchar(':');
+
+			int m;
+
+			for (m = 0; m <= 5; m++)
+			{
+				_putchar(m);
+			}
+
+			int s;
+
+			for (s = 0; s <= 9; s++)
+			{
+				_putchar(s);
+			}
+			_putchar('\n');
+		}
 }
