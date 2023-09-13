38f0a5cc9d19310fae8491e55b9e1fe5c271437c
Imtiyaziy <hitzamiy@gmail.com>
2023-09-12 17:55:07 -0700

    r

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *_abs - display point
+ *@n : The charact to print
+ *
+ *Return: 0 for success
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		int abs = n(-1);
+
+		return (abs);
+	}
+
+	return (j);
+}
