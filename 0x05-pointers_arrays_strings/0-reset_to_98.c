10083ef97f1723f1fb63ce48bf76ff184fb020a6
Imtiyaziy <hitzamiy@gmail.com>
2023-09-18 10:04:24 -0700

    reset value of n

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,10 +3,11 @@
 /**
  * reset_to_98 - a function that takes a pointer to an int as parameter
  * and updates the value it points to to 98
- @*n : The character to input
+ @n : The character to input
  *Return: n
  */
 void reset_to_98(int *n)
 {
+	int n;
 	*n = 98;
 }
