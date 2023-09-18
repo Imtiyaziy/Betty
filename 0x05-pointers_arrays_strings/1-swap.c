9e72c20e2efdb101d2580156e1fd91b4ed3f0b1c
Imtiyaziy <hitzamiy@gmail.com>
2023-09-18 10:32:33 -0700

    reset value of n

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *swap_int - a function that swaps the values of two integers
+ *@b: First input
+ *@b: Second input
+ *
+ */
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	*a = c;
+	*b = *a;
+	c = *b;
+}
